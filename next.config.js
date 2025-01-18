b9545bb471bb5b22290bac527497a97c286dd7ac
Kenneth Auchenberg <kenneth@auchenberg.dk>
2025-01-18 10:38:19 -0500

    Get rid of Next warnings

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['img.logo.dev'],
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: '**.githubusercontent.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'img.logo.dev',
+            }
+        ],
     },
     webpack: (config) => {
         config.module.rules.push({
